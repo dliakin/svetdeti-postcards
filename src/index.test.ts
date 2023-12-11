bc20d579016e668d7382e2a1a5ae0816a569a492
Dmitrii Liakin <d.lyakin@gmail.com>
2023-12-11 13:04:46 -0800

    Refactoring test file

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,43 +1,43 @@
 import request from "supertest";
 import pdfParse from "pdf-parse";
-import { startServer } from "./index"; // Импорт функции startServer из вашего файла сервера
+import { startServer } from "./index"; // Import the startServer function from your server file
 import type { Server } from "http";
 
 describe("POST /submit-letter PDF Generation", () => {
   let server: Server;
 
   beforeAll(() => {
-    // Запуск сервера на определенном порту
+    // Start the server on a specific port
     server = startServer(3000);
   });
 
   afterAll((done) => {
-    // Закрытие сервера после выполнения тестов
+    // Close the server after tests are executed
     server.close(done);
   });
 
   it("should generate a PDF with the submitted data", async () => {
-    // Тестовые данные для отправки
+    // Test data for submission
     const testData = {
       email: "example@example.com",
       name: "John Doe",
-      letterText: "Пример текста письма",
-      momentGift: "Подаренный момент",
-      signature: "Подпись",
+      letterText: "Sample letter text",
+      momentGift: "Gifted moment",
+      signature: "Signature",
     };
 
-    // Отправка POST-запроса с тестовыми данными
+    // Send POST request with test data
     const response = await request(server).post("/submit-letter").send(testData).expect(200).expect("Content-Type", /pdf/);
 
-    // Проверка, что в ответе есть содержимое
+    // Check that the response has content
     expect(response.body).toBeDefined();
     expect(response.body.length).toBeGreaterThan(0);
 
-    // Анализ полученного PDF
+    // Analyze the received PDF
     const pdfData = await pdfParse(response.body);
     const pdfText = pdfData.text;
 
-    // Проверка наличия тестовых данных в тексте PDF
+    // Check for the presence of test data in the PDF text
     expect(pdfText).toContain(testData.name);
     expect(pdfText).toContain(testData.letterText);
     expect(pdfText).toContain(testData.momentGift);
